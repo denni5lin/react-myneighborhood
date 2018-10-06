de66e96e8436095d79791fab013d773ddbbbf6cc
Dennis Lin <lin.dennis18@gmail.com>
2018-10-06 09:10:58 -0700

    Add search field to sidebar w/o functionality

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -5,6 +5,7 @@ class SideBar extends Component {
   render() {
     return (
       <div className="sideBar">
+        <input type={"search"} id={"search"} placeholder={"Filter Venues"} />
         <VenueList />
       </div>
     );
