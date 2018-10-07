214cf9c3d884217ef75d586121e6c9e77c0bb80f
Dennis Lin <lin.dennis18@gmail.com>
2018-10-07 12:09:47 -0700

    Add category icon to list item

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -7,6 +7,14 @@ class ListItem extends Component {
         className="listItem"
         onClick={() => this.props.handleListItemClick(this.props)}
       >
+        <img
+          src={
+            this.props.categories[0].icon.prefix +
+            "32" +
+            this.props.categories[0].icon.suffix
+          }
+          alt={this.props.categories[0].name}
+        />
         {this.props.name}
       </li>
     );
