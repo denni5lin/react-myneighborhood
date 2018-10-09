5eaecfd69b9f6f937782c2ce2191ccc46b220c99
Dennis Lin <lin.dennis18@gmail.com>
2018-10-08 20:46:56 -0700

    Add marker animation

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,3 +1,4 @@
+/* global google */
 import React, { Component } from "react";
 
 import {
@@ -17,31 +18,40 @@ const MyMapComponent = withScriptjs(
       center={props.center}
     >
       {props.markers &&
-        props.markers.filter(marker => marker.isVisible).map((marker, idx) => {
-          const venueInfo = props.venues.find(venue => venue.id === marker.id);
-          return (
-            <Marker
-              key={idx}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              onClick={() => props.handleMarkerClick(marker)}
-            >
-              {marker.isOpen &&
-                venueInfo.bestPhoto && (
-                  <InfoWindow>
-                    <React.Fragment>
-                      <img
-                        src={`${venueInfo.bestPhoto.prefix}200x200${
-                          venueInfo.bestPhoto.suffix
-                        }`}
-                        alt={"Venue"}
-                      />
-                      <p>{venueInfo.name}</p>
-                    </React.Fragment>
-                  </InfoWindow>
-                )}
-            </Marker>
-          );
-        })}
+        props.markers
+          .filter(marker => marker.isVisible)
+          .map((marker, idx, arr) => {
+            const venueInfo = props.venues.find(
+              venue => venue.id === marker.id
+            );
+            return (
+              <Marker
+                key={idx}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                onClick={() => props.handleMarkerClick(marker)}
+                animation={
+                  arr.length === 1
+                    ? google.maps.Animation.BOUNCE
+                    : google.maps.Animation.DROP
+                }
+              >
+                {marker.isOpen &&
+                  venueInfo.bestPhoto && (
+                    <InfoWindow>
+                      <React.Fragment>
+                        <img
+                          src={`${venueInfo.bestPhoto.prefix}200x200${
+                            venueInfo.bestPhoto.suffix
+                          }`}
+                          alt={"Venue"}
+                        />
+                        <p>{venueInfo.name}</p>
+                      </React.Fragment>
+                    </InfoWindow>
+                  )}
+              </Marker>
+            );
+          })}
     </GoogleMap>
   ))
 );
