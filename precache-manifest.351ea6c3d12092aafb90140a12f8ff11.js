72cdd801aae5696249bf299a99e7eeb0abc89d66
Dennis Lin <lin.dennis18@gmail.com>
2018-10-13 10:25:18 -0700

    Updates

---
+++ precache-manifest.351ea6c3d12092aafb90140a12f8ff11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e31daa526491b57b3da8",
+    "url": "/react-myneighborhood/static/js/runtime~main.e31daa52.js"
+  },
+  {
+    "revision": "5ad772cfd5b188263613",
+    "url": "/react-myneighborhood/static/js/main.5ad772cf.chunk.js"
+  },
+  {
+    "revision": "3e82109e0ab8703899c3",
+    "url": "/react-myneighborhood/static/js/1.3e82109e.chunk.js"
+  },
+  {
+    "revision": "5ad772cfd5b188263613",
+    "url": "/react-myneighborhood/static/css/main.c2aa8b2c.chunk.css"
+  },
+  {
+    "revision": "65f0cde93d99d97aceaece9eaa1f8903",
+    "url": "/react-myneighborhood/index.html"
+  }
+];
